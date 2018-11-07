b5e38d6550068f7bdc1322eede26bfe62d3accb8
jyzhou95 <41101438+jyzhou95@users.noreply.github.com>
2018-11-06 21:02:19 -0500

    Initial data merging script

---
+++ read_data.R
@@ -0,0 +1,39 @@
+library(data.table)
+
+dt.categories <- fread("~/GitHub/edav_final_project/health_care_data/categories.csv")
+dt.catorgs <- fread("~/GitHub/edav_final_project/health_care_data/catsorgs.csv")
+dt.orgsindex <- fread("~/GitHub/edav_final_project/health_care_data/orgsindex.csv")
+dt.prcbreaches <- fread("~/GitHub/edav_final_project/health_care_data/prcbreaches2005-18.csv")
+
+dt.categories_final <- dt.categories[,list(cat_id = CatID, 
+                                     cat_name = CatName)]
+
+dt.catorgs_final <- dt.catorgs[,list(cat_id = CatID, 
+                               org_id = OrgID, 
+                               type = Type)]
+
+dt.orgsindex_final <- dt.orgsindex[,list(org_id = OrgID,
+                                   company_name = Name,
+                                   source_type = SourceType)]
+
+dt.prcbreaches_final <- dt.prcbreaches[,list(records_breached = Records.Breached,
+                                       records_breached_detail = Records.Breached...Detail, 
+                                       name = Name, 
+                                       cat_id = CatID, 
+                                       total_records = Total.Records,
+                                       region = Region, 
+                                       category = Category_dm, 
+                                       entity_type = Entity_prc, 
+                                       state = State, 
+                                       org_id = OrgID, 
+                                       location = Location, 
+                                       dt = Date.Made.Public, 
+                                       breach_source = Source.of.Breach.Notification, 
+                                       breach_type = Type)]
+
+dt.master <- merge(merge(merge(dt.prcbreaches_final, dt.catorgs_final, by = c("cat_id", "org_id"), all = TRUE),
+                   dt.categories_final, by = c("cat_id"), all = TRUE),
+                   dt.orgsindex_final, by = c("org_id"), all = TRUE)
+
+
+
