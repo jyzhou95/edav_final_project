fd07e9bca9eee121f8e41f3cea7cf16e8455e3b1
Michelle Chen <mc9ds@virginia.edu>
2018-11-14 11:43:10 -0500

    Yimin, Michelle Exploration Parts Added

--- Michelle_exploration.R
+++ Michelle_exploration.R
@@ -1,5 +1,7 @@
 library(data.table)
 library(glue)
+library(tidyverse)
+library(vcd)
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 parent_dir <- getwd()
@@ -10,57 +12,39 @@ dt.orgsindex <- fread(glue("{parent_dir}/health_care_data/orgsindex.csv"))
 dt.prcbreaches <- fread(glue("{parent_dir}/health_care_data/prcbreaches2005-18.csv"))
 
 dt.categories_final <- dt.categories[,list(cat_id = CatID, 
-                                     cat_name = CatName)]
+                                           cat_name = CatName)]
 
 dt.catorgs_final <- dt.catorgs[,list(cat_id = CatID, 
-                               org_id = OrgID, 
-                               type = Type)]
+                                     org_id = OrgID, 
+                                     type = Type)]
 
 dt.orgsindex_final <- dt.orgsindex[,list(org_id = OrgID,
-                                   company_name = Name,
-                                   source_type = SourceType)]
+                                         company_name = Name,
+                                         source_type = SourceType)]
 
 dt.prcbreaches_final <- dt.prcbreaches[,list(records_breached = Records.Breached,
-                                       records_breached_detail = Records.Breached...Detail, 
-                                       name = Name, 
-                                       cat_id = CatID, 
-                                       total_records = Total.Records,
-                                       region = Region, 
-                                       category = Category_dm, 
-                                       entity_type = Entity_prc, 
-                                       state = State, 
-                                       org_id = OrgID, 
-                                       location = Location, 
-                                       dt = Date.Made.Public, 
-                                       breach_source = Source.of.Breach.Notification, 
-                                       breach_type = Type)]
+                                             records_breached_detail = Records.Breached...Detail, 
+                                             name = Name, 
+                                             cat_id = CatID, 
+                                             total_records = Total.Records,
+                                             region = Region, 
+                                             category = Category_dm, 
+                                             entity_type = Entity_prc, 
+                                             state = State, 
+                                             org_id = OrgID, 
+                                             location = Location, 
+                                             dt = Date.Made.Public, 
+                                             breach_source = Source.of.Breach.Notification, 
+                                             breach_type = Type)]
 
 dt.orgsindex_final <- unique(dt.orgsindex_final, by = c("org_id"))
 
 dt.master <- merge(merge(merge(dt.prcbreaches_final, dt.catorgs_final, by = c("cat_id", "org_id"), all.x = TRUE),
-                   dt.categories_final, by = c("cat_id"), all.x = TRUE),
+                         dt.categories_final, by = c("cat_id"), all.x = TRUE),
                    dt.orgsindex_final, by = c("org_id"), all.x = TRUE)
 
-# Fix region
-dt.region <- unique(dt.master[!is.na(region)][,list(region, state)])
+dt.master
 
-# Merge it backc on
-dt.master[,region := NULL]
-dt.master <- merge(dt.master, dt.region, by = c("state"), all.x = TRUE)
-
-# Add unique identifier to each breach
-dt.master$breach_id <- 1:nrow(dt.master)
-
-dt.date_format1 <- dt.master[grepl("\\-", dt)]
-dt.date_format1$dt <- as.Date(dt.date_format1$dt, format = "%d-%b-%y")
-
-dt.date_format2 <- dt.master[grepl("\\/", dt)]
-dt.date_format2$dt <- as.Date(dt.date_format2$dt, format = "%m/%d/%Y")
-
-dt.final <- rbind(dt.date_format1,
-                  dt.date_format2)
-
-write.csv(dt.final, glue("{parent_dir}/health_care_data/master.csv"))
 # Entity type
 # BSO - business other
 # BSF - business - financial and insurance services
@@ -69,6 +53,7 @@ write.csv(dt.final, glue("{parent_dir}/health_care_data/master.csv"))
 # GOV - government and military
 # MED - healthcare - medical providersr
 # NGO - nonprofit organizations
+
 
 ########################
 #Michelle Exploration###
@@ -121,7 +106,4 @@ physician_cat_breach<-ggplot(data = counts3,aes(x = reorder(breach_type,Freq), y
   ggtitle("Frequency of each Breach_Type for Physician, Hospital Category") + 
   ylab("Frequency of Breach Type (Not # records breached)") + xlab("")
 physician_cat_breach
-
-
-
 
