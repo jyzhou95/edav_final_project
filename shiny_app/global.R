de9cbcf9cd640fbfad80d0573ac2f5787d698e52
jyzhou95 <41101438+jyzhou95@users.noreply.github.com>
2018-12-02 23:56:33 -0500

    Removed choroplethr and rvest dependency

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -3,14 +3,11 @@ library(data.table)
 library(ggplot2)
 library(lubridate)
 library(plotly)
-library(choroplethr)
-library(rvest)
 library(ggthemes)
 library(viridis)
 
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-parent_dir <- getwd()
 setwd("..")
 parent_dir <- getwd()
 dt.data <- fread(paste0(parent_dir, "/health_care_data/master.csv"))
@@ -29,16 +26,5 @@ dt.breach_type_name <- data.table(breach_type = c("DISC", "HACK", "CARD", "INSD"
 dt.data <- merge(dt.data, dt.breach_type_name, by = c("breach_type"))
 
 # Scrape data from Wikipedia
-url <- "http://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_population"
-population <- url %>%
-  read_html() %>%
-  html_nodes(xpath='//*[@id="mw-content-text"]/div/table[1]') %>%
-  html_table()
-population <- population[[1]]
-dt.state_population <- data.table(population)
-dt.state_population <- dt.state_population[,c(3,4)]
-colnames(dt.state_population) <- c("state", "population")
-dt.state_population <- dt.state_population[state %in% state.name]
-dt.state_population[,population := gsub(",", "", population)]
-dt.state_population[,population := as.numeric(population)]
-
+dt.state_population <- fread(paste0(parent_dir, "/health_care_data/state_population.csv"))
+dt.state_population[,V1 := NULL]
